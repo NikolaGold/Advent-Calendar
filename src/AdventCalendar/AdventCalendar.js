3cea46359c62a118125408924a110e55bf6cbbb2
NikolaGold <nikola.goldova1@gmail.com>
2018-12-31 14:19:04 +0100

    refactor AdventCalendar and CalendarItem

--- src/AdventCalendar/AdventCalendar.js
+++ src/AdventCalendar/AdventCalendar.js
@@ -1,50 +1,46 @@
 import React, { Component } from 'react';
 import CalendarItem from './CalendarItem/CalendarItem.js';
 import items from './Items/Items.js';
-
 import './AdventCalendar.css';
 
+class AdventCalendar extends Component {
 
-class AdventCalendar extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			modalIsOpen: false,
-			modalContent: <div>AHOJ</div>,
-			headingContent: ""
+			modalContent: '',
+			headingContent: ''
 		};
 	}
-	render(){
+
+	render() {
 		return(
 			<div className="calendar-container">
 				{this.renderCalendarItems()}
 				{this.state.modalIsOpen && <div id="modal">
-					<button type="button" className="close" aria-label="Close" id="close" onClick={() => this.closeModal()}>x</button>
+					<button type="button" className="close" aria-label="Close" id="close" 
+							onClick={() => this.closeModal()}>x</button>
 					<h1 className="text-center">{this.state.headingContent}</h1>
 					<p className="text-center">{this.state.modalContent}</p>
 				</div>}
 			</div>
-			)
+		)
 	}
 
 	closeModal() {
 		this.setState({modalIsOpen:false});
-		console.log('close');
-
 	}
 
-	openModal(heading,content){
+	openModal(heading,content) {
 		this.setState({modalIsOpen:true, headingContent: heading, modalContent: content});
-		console.log('open');
 	}
 
-	renderCalendarItems(){
-		return items.map((item)=> <CalendarItem onModalClick={(heading,content) => this.openModal(heading,content)} item={item}/>);
-		
+	renderCalendarItems() {
+		return items.map((item) => <CalendarItem key={item.day} 
+				onModalClick={(heading,content) => this.openModal(heading,content)} item={item}/>);	
 	}
-
 
 }
-
 
 export default AdventCalendar;
--- src/AdventCalendar/CalendarItem/CalendarItem.js
+++ src/AdventCalendar/CalendarItem/CalendarItem.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import './CalendarItem.css';
 
-
-class CalendarItem extends Component{
+class CalendarItem extends Component {
 	render(){
 		const content = this.props.item.content;
 		const heading = this.props.item.heading;
 
 		return(
 			<button className="item-container" onClick={() => this.props.onModalClick(heading,content)}>
-
-				<span>{this.props.item["day"]}</span>
-			
+				<span>{this.props.item.day}</span>
 			</button>
 		)
 	}
-
 }
-
 
 export default CalendarItem;
